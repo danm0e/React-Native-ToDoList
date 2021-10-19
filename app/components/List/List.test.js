87340be1fc1199d53c1cb023fd7a75469142ef2d
Dan Moe <danmoe@gmail.com>
2021-10-19 18:34:14 +0100

    Add List tests

---
+++ app/components/List/List.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import List from './List';
+
+const mockGoals = [
+	{ id: 'some-id-1', value: 'some value 1' },
+	{ id: 'some-id-2', value: 'some value 2' }
+]
+
+describe('components/List', () => {
+	describe('When the component is rendered', () => {
+		describe('And there are goals set', () => {
+			test('It should render the correct number of list items', () => {
+				const { getAllByText } = render(<List goals={mockGoals} />)
+				const listItems = getAllByText(/some value/i)
+
+				expect(listItems).toHaveLength(2)
+			})
+		})
+
+		describe('And there are no goals set', () => {
+			test('It should render the empty message', () => {
+				const { getByText, debug } = render(<List goals={[]} />)
+				const emptyMessage = getByText(/No goals/i)
+
+				expect(emptyMessage).toBeDefined()
+			})
+		})
+	})
+})
